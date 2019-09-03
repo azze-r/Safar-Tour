3b4cdd82fa680f379cc459df5070f154648f7219
Jérémy Mulet <mulet.jeremy@gmail.com>
2019-09-03 21:45:37 +0200

    remove third tab

--- app/src/main/java/com/bolo/bolomap/ui/travelog/TravelogViewModel.kt
+++ app/src/main/java/com/bolo/bolomap/ui/travelog/TravelogViewModel.kt
@@ -1,13 +1,13 @@
-package com.bolo.bolomap.ui.notifications
+package com.bolo.bolomap.ui.travelog
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class TravelogViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
