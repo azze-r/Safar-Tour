9c0f86200801dadfd1d686d752a736c39e970331
jeremy <jeremy@bestasset.eu>
2019-09-04 10:39:16 +0200

    remove third tab

--- app/src/main/java/com/bolo/bolomap/ui/travelog/TravelogFragment.kt
+++ app/src/main/java/com/bolo/bolomap/ui/travelog/TravelogFragment.kt
@@ -1,4 +1,4 @@
-package com.bolo.bolomap.ui.notifications
+package com.bolo.bolomap.ui.travelog
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bolo.bolomap.R
 
-class NotificationsFragment : Fragment() {
+class TravelogFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var travelogViewModel: TravelogViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
+        travelogViewModel =
+            ViewModelProviders.of(this).get(TravelogViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        travelogViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
