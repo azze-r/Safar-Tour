a3e8c55a11085eb1a7ece60c0b70ff96c4ef106f
jeremy <jeremy@bestasset.eu>
2019-09-04 10:20:43 +0200

    Revert "remove third tab"
    This reverts commit 3b4cdd82fa680f379cc459df5070f154648f7219.

--- app/src/main/java/com/bolo/bolomap/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/bolo/bolomap/ui/notifications/NotificationsFragment.kt
@@ -1,4 +1,4 @@
-package com.bolo.bolomap.ui.travelog
+package com.bolo.bolomap.ui.notifications
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bolo.bolomap.R
 
-class TravelogFragment : Fragment() {
+class NotificationsFragment : Fragment() {
 
-    private lateinit var travelogViewModel: TravelogViewModel
+    private lateinit var notificationsViewModel: NotificationsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        travelogViewModel =
-            ViewModelProviders.of(this).get(TravelogViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_travelog, container, false)
-        val textView: TextView = root.findViewById(R.id.text_travelog)
-        travelogViewModel.text.observe(this, Observer {
+        notificationsViewModel =
+            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/bolo/bolomap/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/bolo/bolomap/ui/notifications/NotificationsViewModel.kt
@@ -1,13 +1,13 @@
-package com.bolo.bolomap.ui.travelog
+package com.bolo.bolomap.ui.notifications
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class TravelogViewModel : ViewModel() {
+class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
