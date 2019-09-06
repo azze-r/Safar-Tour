8d7977e0aaf8f5d731cc8c83c8733e75a5519bc4
Azedine <azedine.rouani@bestasset.eu>
2019-09-06 15:56:53 +0100

    display intent photo and display uri from this photos (real one and virtal one)

--- app/src/main/java/com/bolo/bolomap/ui/map/MapFragment.kt
+++ app/src/main/java/com/bolo/bolomap/ui/map/MapFragment.kt
@@ -1,12 +1,17 @@
 package com.bolo.bolomap.ui.map
 
+import android.app.Activity.RESULT_OK
 import android.content.Context
+import android.content.Intent
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -21,6 +26,8 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_home.*
+import android.net.Uri
+import java.io.ByteArrayOutputStream
 
 
 class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
@@ -35,6 +42,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
     lateinit var fab2: FloatingActionButton
     lateinit var fab3: FloatingActionButton
     var isFABOpen = false
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,6 +65,10 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
             } else {
                 closeFABMenu()
             }
+        }
+
+        fab2.setOnClickListener {
+            dispatchTakePictureIntent()
         }
 
         mMapView!!.onCreate(savedInstanceState)
@@ -161,6 +173,49 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         background.draw(canvas)
         vectorDrawable.draw(canvas)
         return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            activity?.packageManager?.let {
+                takePictureIntent.resolveActivity(it)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data.extras?.get("data") as Bitmap
+            val tempUri = context?.let { getImageUri(it, imageBitmap) }
+
+            Toast.makeText(context,tempUri.toString(),Toast.LENGTH_LONG).show()
+            Toast.makeText(context, tempUri?.let { getRealPathFromURI(it) },Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    private fun getImageUri(inContext:Context, inImage:Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(inContext.contentResolver, inImage, "Title", null)
+        return Uri.parse(path)
+    }
+
+    private fun getRealPathFromURI(uri:Uri): String {
+        var path = ""
+        if (activity?.contentResolver != null) {
+            val cursor = activity?.contentResolver!!.query(uri, null, null, null, null)
+            if (cursor != null) {
+                cursor.moveToFirst()
+                val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+                path = cursor.getString(idx)
+                cursor.close()
+            }
+        }
+        return path
     }
 
 }
